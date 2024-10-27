696023c309c702b41467540b6329739b79d5ef1a
ktgitfrog333 <paraysis+git@gmail.com>
2024-10-27 12:44:10 +0900

    ▼UI・サウンド 　●チュートリアル実装_2 セレクトシーンにて簡易チュートリアルを削除
    ▼共有事項
    　●セレクトのキャンバス（Canvas.prefab）にて、Tutorialを無効化
    　●セレクトシーンにて、キャンバス（Canvas.prefab）にて、Tutorialを無効化

--- DJ_Onmyoji_AKA_AbeNoSeimei/Assets/Select/Scripts/Presenter/SelectPresenter.cs
+++ DJ_Onmyoji_AKA_AbeNoSeimei/Assets/Select/Scripts/Presenter/SelectPresenter.cs
@@ -28,12 +28,6 @@ namespace Select.Presenter
         [SerializeField] private StageSelectModel stageSelectModel;
         /// <summary>プレゼンタの読み込み完了</summary>
         public IReactiveProperty<bool> IsCompleted { get; private set; } = new BoolReactiveProperty();
-        /// <summary>チュートリアル画面のビュー</summary>
-        [SerializeField] private TutorialView tutorialView;
-        /// <summary>チュートリアル画面ページのビュー</summary>
-        [SerializeField] private TutorialViewPageView[] tutorialViewPageViews;
-        /// <summary>遊び方確認ページのモデル</summary>
-        [SerializeField] private TutorialViewPageModel[] tutorialViewPageModels;
         /// <summary>遅延処理</summary>
         private Tween _tween;
 
@@ -44,27 +38,11 @@ namespace Select.Presenter
             fadeImageModel = GameObject.Find("FadeImage").GetComponent<FadeImageModel>();
             stageSelectView = GameObject.Find("StageSelect").GetComponent<StageSelectView>();
             stageSelectModel = GameObject.Find("StageSelect").GetComponent<StageSelectModel>();
-            tutorialView = GameObject.Find("Tutorial").GetComponent<TutorialView>();
-            var gmvPageVIdx = 0;
-            var gmvPageMIdx = 0;
-            tutorialViewPageViews = new TutorialViewPageView[5];
-            tutorialViewPageModels = new TutorialViewPageModel[5];
-            tutorialViewPageViews[gmvPageVIdx++] = GameObject.Find("TutorialViewPage_1").GetComponent<TutorialViewPageView>();
-            tutorialViewPageModels[gmvPageMIdx++] = GameObject.Find("TutorialViewPage_1").GetComponent<TutorialViewPageModel>();
-            tutorialViewPageViews[gmvPageVIdx++] = GameObject.Find("TutorialViewPage_2").GetComponent<TutorialViewPageView>();
-            tutorialViewPageModels[gmvPageMIdx++] = GameObject.Find("TutorialViewPage_2").GetComponent<TutorialViewPageModel>();
-            tutorialViewPageViews[gmvPageVIdx++] = GameObject.Find("TutorialViewPage_3").GetComponent<TutorialViewPageView>();
-            tutorialViewPageModels[gmvPageMIdx++] = GameObject.Find("TutorialViewPage_4").GetComponent<TutorialViewPageModel>();
-            tutorialViewPageViews[gmvPageVIdx++] = GameObject.Find("TutorialViewPage_4").GetComponent<TutorialViewPageView>();
-            tutorialViewPageModels[gmvPageMIdx++] = GameObject.Find("TutorialViewPage_4").GetComponent<TutorialViewPageModel>();
-            tutorialViewPageViews[gmvPageVIdx++] = GameObject.Find("TutorialViewPage_5").GetComponent<TutorialViewPageView>();
-            tutorialViewPageModels[gmvPageMIdx++] = GameObject.Find("TutorialViewPage_5").GetComponent<TutorialViewPageModel>();
         }
 
         public void OnStart()
         {
             // ステージ番号を取得する処理を追加する
-            tutorialView.gameObject.SetActive(false);
             SelectGameManager.Instance.AudioOwner.PlayBGM(ClipToPlayBGM.bgm_select);
             // ステージ番号を取得する処理を追加する
             var saveDatas = SelectGameManager.Instance.SceneOwner.GetSaveDatas();
@@ -76,37 +54,6 @@ namespace Select.Presenter
                     // UI操作を許可
                     if (!stageSelectModel.SetButtonAndEventTriggerEnabled(true))
                         Debug.LogError("SetButtonAndEventTriggerEnabled");
-                    switch (stageIndex.Value)
-                    {
-                        case 1:
-                            _tween = DOVirtual.DelayedCall(0.5f, () =>
-                            {
-                                if (!tutorialView.gameObject.activeSelf)
-                                {
-                                    // チュートリアル画面の表示
-                                    tutorialView.gameObject.SetActive(true);
-                                    if (!tutorialView.SetPage(EnumTutorialPagesIndex.Page_1))
-                                        Debug.LogError("ページ変更呼び出しの失敗");
-                                    tutorialViewPageModels[(int)EnumTutorialPagesIndex.Page_1].SetSelectedGameObject();
-                                }
-                            });
-
-                            break;
-                        default:
-                            _tween = DOVirtual.DelayedCall(5.0f, () =>
-                            {
-                                if (!tutorialView.gameObject.activeSelf)
-                                {
-                                    // チュートリアル画面の表示
-                                    tutorialView.gameObject.SetActive(true);
-                                    if (!tutorialView.SetPage(EnumTutorialPagesIndex.Page_1))
-                                        Debug.LogError("ページ変更呼び出しの失敗");
-                                    tutorialViewPageModels[(int)EnumTutorialPagesIndex.Page_1].SetSelectedGameObject();
-                                }
-                            });
-
-                            break;
-                    }
                 })
                 .AddTo(gameObject);
             foreach (var eventState in stageSelectModel.EventStates.Select((p, i) => new { Content = p, Index = i }))
@@ -120,69 +67,6 @@ namespace Select.Presenter
             }
             // ロードの完了
             IsCompleted.Value = true;
-            // チュートリアル画面の操作
-            for (var i = 0; i < tutorialViewPageModels.Length; i++)
-            {
-                var tmpIdx = i;
-                tutorialViewPageModels[tmpIdx].EventState.ObserveEveryValueChanged(x => x.Value)
-                .Subscribe(x =>
-                {
-                    switch ((EnumEventCommand)x)
-                    {
-                        case EnumEventCommand.Default:
-                            // 処理なし
-                            break;
-                        case EnumEventCommand.Selected:
-                            SelectGameManager.Instance.AudioOwner.PlaySFX(ClipToPlay.se_select);
-                            if (!tutorialView.PlayPagingAnimation((EnumTutorialPagesIndex)tmpIdx))
-                                Debug.LogError("ページ変更アニメーション呼び出しの失敗");
-                            break;
-                        case EnumEventCommand.DeSelected:
-                            // 処理なし
-                            break;
-                        case EnumEventCommand.Submited:
-                            // 処理なし
-                            break;
-                        case EnumEventCommand.Canceled:
-                            SelectGameManager.Instance.AudioOwner.PlaySFX(ClipToPlay.se_cancel);
-                            if (!tutorialViewPageModels[tmpIdx].SetButtonEnabled(false))
-                                Debug.LogError("ボタン有効／無効切り替え呼び出しの失敗");
-                            if (!tutorialViewPageModels[tmpIdx].SetEventTriggerEnabled(false))
-                                Debug.LogError("イベント有効／無効切り替え呼び出しの失敗");
-                            // チュートリアル画面クローズのアニメーション
-                            Observable.FromCoroutine<bool>(observer => tutorialView.PlayCloseAnimation(observer))
-                                .Subscribe(_ =>
-                                {
-                                    tutorialView.gameObject.SetActive(false);
-                                    if (!stageSelectModel.SetSelectedGameObjectOfAreaContentModels(stageIndex.Value))
-                                        Debug.LogError("SetSelectedGameObjectOfAreaContentModels");
-                                })
-                                .AddTo(gameObject);
-                            break;
-                    }
-                });
-            }
-
-            // uGUI制御（有効）
-            this.UpdateAsObservable()
-                .Select(_ => SelectGameManager.Instance)
-                .Where(x => x != null)
-                .Select(x => x.InputSystemsOwner)
-                .Subscribe(x =>
-                {
-                    if (x.InputUI.Paused)
-                    {
-                        if (!tutorialView.gameObject.activeSelf)
-                        {
-                            // チュートリアル画面の表示
-                            SelectGameManager.Instance.AudioOwner.PlaySFX(ClipToPlay.se_decided);
-                            tutorialView.gameObject.SetActive(true);
-                            if (!tutorialView.SetPage(EnumTutorialPagesIndex.Page_1))
-                                Debug.LogError("ページ変更呼び出しの失敗");
-                            tutorialViewPageModels[(int)EnumTutorialPagesIndex.Page_1].SetSelectedGameObject();
-                        }
-                    }
-                });
         }
     }
 }
